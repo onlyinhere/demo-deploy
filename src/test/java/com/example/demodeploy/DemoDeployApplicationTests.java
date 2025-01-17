d071a8b4820283557eb3f6fa2a873486b9db8ddc
onlyinhere3113 <onlyinhere3113@gmail.com>
2025-01-17 11:46:58 +0900

    feat: CI/CD gitaction 빌드 작업 추가, 단위 테스트 호출여부, 빌드 결과물 이동 결과, ec2 업로드

--- src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
+++ src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
@@ -11,6 +11,7 @@ class DemoDeployApplicationTests {
         // 단위 테스트 (하는 자리)
         // MVC 테스트 -> 목업통해서 가상 처리 가능
         // RestAPI 테스트 가능
+        // 메세지가 보이지는 않지마느 차후 코드로 @@
         System.out.println("단위 테스트 호출!! echo");
     }
 
